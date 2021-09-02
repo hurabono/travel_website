82c4b95613b23005069e57658e627de4af84de89
Kaya <love4stephanie@protonmail.com>
2021-09-02 00:23:32 -0400

    Sept 01 2021 study

---
+++ src/components/Footer.js
@@ -0,0 +1,135 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+
+function Footer() {
+  return (
+    // EMAIL SUBSCRIPTION SECTION
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newletter to receive our best vaction deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscription at any time
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              className="footer-input"
+              name="email"
+              type="email"
+              placeholder="Your email"
+            />
+
+            <Button buttonStyle="btn__outline">SUBSCRIBE</Button>
+          </form>
+        </div>
+      </section>
+
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/">Contact</Link>
+            <Link to="/">Support</Link>
+            <Link to="/">Destinations</Link>
+            <Link to="/">Sponsorships</Link>
+          </div>
+
+          <div className="footer-link-items">
+            <h2>Videos</h2>
+            <Link to="/">Agency</Link>
+            <Link to="/">Submit Video</Link>
+            <Link to="/">Ambassadors</Link>
+            <Link to="/">Influencer</Link>
+          </div>
+
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+
+          <div className="footer-link-items">
+            <h2>Social Media</h2>
+            <Link to="/">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+
+      <section className="social-media">
+        <div className="social=media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRIPF
+              <i className="fas fa-plane-departure" />
+            </Link>
+          </div>
+          <small class="website-rights">TRIPF © 2021</small>
+          <div class="social-icons">
+            <Link
+              class="social-icon-link facebook"
+              to="/"
+              target="_blank"
+              aria-label="Facebook"
+            >
+              <i class="fab fa-facebook-f" />
+            </Link>
+            <Link
+              class="social-icon-link instagram"
+              to="/"
+              target="_blank"
+              aria-label="Instagram"
+            >
+              <i class="fab fa-instagram" />
+            </Link>
+            <Link
+              class="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i class="fab fa-youtube" />
+            </Link>
+            <Link
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i class="fab fa-twitter" />
+            </Link>
+            <Link
+              class="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i class="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
